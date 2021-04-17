2ca4fb38f3ab0a010462035b14f099bdf2094550
michaelmoreno <michaelmoreno.dev@gmail.com>
2021-04-17 08:17:38 -0500

    Upload.

--- src/boid.js
+++ src/boid.js
@@ -4,26 +4,7 @@ export class Boid {
     this.vel = vel
     this.accel = accel;
   }
-  update() {
-    this.pos.add(this.vel)
-    console.log(this.pos);
-  }
   draw() {
-    const vertices = [
-      [this.x - 8, this.y + 8],
-      [this.x, this.y - 8],
-      [this.x + 8, this.y + 8],
-    ];
-
     c.beginPath();
-    vertices.forEach(v => {
-      if (v == vertices[0])
-        c.moveTo(v[0], v[1]);
-      else
-        c.lineTo(v[0], v[1]);
-    })
-    c.closePath();
-    c.fillStyle = '#ccc';
-    c.fill();
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
-import { Vector2D } from './custom';
-import { Boid } from './boid';
-import "style.scss";
 
-const testV = new Vector2D(5, 3);
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
-// canvas.style.backgroundColor = 'black';
+import { Vector2D } from './custom';
+import { Boid } from './boid';
 function size() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
@@ -13,10 +10,7 @@ function size() {
 ;
 size();
 window.addEventListener('resize', size());
-// ['load', 'resize'].forEach(e => window.addEventListener(e, function() {
-//   canvas.width = window.innerWidth;
-//   canvas.height = window.innerHeight;
-// }))
+
 const boids = [];
 function init() {
     for (let i = 0; i < 1; i++) {
